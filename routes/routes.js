175c1af104a0b2614f1cedf7fe10fa5c597c8b84
Adriana Sampaio <adrianadelimasampaio@gmail.com>
2022-04-20 16:19:10 +0200

    passport

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
 const { registerValidation, loginValidation } = require("./validation");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-const { readlink } = require("fs");
-const User = require("../models/User");
 const authController = require("../controllers/authControllers");
 const favoritesController = require("../controllers/favoritesControllers");
+const passport = require("passport");
 
 router.get("/", (req, res) => {
   res.send("Hello World!");
@@ -13,7 +10,26 @@ router.get("/", (req, res) => {
 
 router.get("/register", authController.getRegisterPage);
 router.post("/register", authController.createUser);
+router.get("/login", authController.getLoginPage);
 router.post("/login", authController.login);
 router.get("/favorites", favoritesController.getFavorites);
+router.delete("/favorites", favoritesController.removeFavorite);
+router.get("/movies", favoritesController.getMovies);
+router.post("/movies", favoritesController.addFavorite);
+
+// function isLoggedIn(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     //a passport function
+//     return next();
+//   }
+//   res.redirect("/movies");
+// }
+
+// function isNotLoggedIn(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     return res.redirect("/");
+//   }
+//   next();
+// }
 
 module.exports = router;
